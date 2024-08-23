e4e7f7afea721dad87fd0a456fcb384836a024b1
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-08-23 15:14:43 +0530

    bubble sort

--- 12.sorting/bubble_sort.java
+++ 12.sorting/bubble_sort.java
@@ -1,3 +1,25 @@
 public class bubble_sort {
-    
+    public static void bubble_sorting(int arr[]){
+        for (int turn=0; turn<arr.length-1; turn++){
+            for (int j=0;j<arr.length-1-turn;j++){
+                if(arr[j] > arr[j+1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+    public static void print_Arr(int arr[]){
+        for(int i=0;i<arr.length; i++){
+            System.out.println(arr[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+        int arr[] = {5,4,1,3,2};
+        bubble_sorting(arr);
+        print_Arr(arr);
+    }
 }
