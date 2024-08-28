a2451a93b3c0a3ad7cb619c5947624f592cce360
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-08-28 22:36:23 +0530

    Create insertion_sort.java

---
+++ 12.sorting/insertion_sort.java
@@ -0,0 +1,9 @@
+public class insertion_sort {
+
+    public static void sort(int arr[]){
+        
+    }
+    public static void main(String[] args) {
+        int[] arr = {5, 2, 8, 1, 9};
+    }
+}
