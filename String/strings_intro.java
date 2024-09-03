a30e96c7454cbb4b163f3335f01973a5365b94f9
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-09-04 00:26:01 +0530

    String Introduction

---
+++ String/strings_intro.java
@@ -0,0 +1,37 @@
+import java.util.*;
+
+public class strings_intro {
+
+    public static void printLetters(String str){
+        for(int i=0;i<str.length();i++){
+            System.out.print(str.charAt(i) + " ");//to print all letters in the string
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        // char arr[] = {'a','b','c','d'};
+        // String str = "abcd";
+        // String str2 = new String("xyz");
+        // //string are IMMUTABLE
+
+        // //input and output
+
+        // Scanner sc = new Scanner(System.in);
+        // String name;
+        // name = sc.nextLine(); //in next function it takes only single words, so we use nextLine function
+        // System.out.println(name);
+
+        //length of the string
+        // String fullName = "Tony Stark";
+        // System.out.println(fullName.length());
+
+        //Concatenation
+        String firstName = "Jay";
+        String lastName = "Kumar";
+        String fullName = firstName + " " + lastName;
+
+        // System.out.println(fullName.charAt(0));
+        printLetters(fullName);
+
+    }
+}
