bdc5a7070a35abce08212ed873aa09ba48a51c69
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-09-06 23:40:02 +0530

    packages in java

---
+++ 15.OOPS/packages.java
@@ -0,0 +1,10 @@
+// package myPackage; //user defined package
+import java.util.*; //inbuilt java package
+
+public class packages {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //here Scanner is clss, sc is object, new is keyword, Scanner() is constructor call, System.in is parameter
+        int a = sc.nextInt(); //sc object ka next int function use ker rehe he
+        System.out.println(a);
+    }
+}
