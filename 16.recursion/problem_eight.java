85ad5e85c3df5d8792be08c595e48f09ed65ed58
jaykumar2004 <106320129+jaykumar2004@users.noreply.github.com>
2024-09-20 20:23:54 +0530

    last occurence of an element in an array

---
+++ 16.recursion/problem_eight.java
@@ -0,0 +1,20 @@
+public class problem_eight {
+    public static int lastOccurence(int arr[], int key, int i){
+        if(i == arr.length){
+            return -1;
+        }
+
+        //Look Forward and check with self:
+        int isFound = lastOccurence(arr, key, i+1);
+        if(isFound == -1 && arr[i] == key){
+            return i;
+        }
+        return isFound;
+
+    }
+    public static void main(String[] args) {
+        int arr [] = {8,3,6,9,5,10,2,5,3};
+        System.out.println("key found at index:" + lastOccurence(arr, 5, 0));
+
+    }
+}
