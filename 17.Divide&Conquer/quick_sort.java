be239d640b57cebb28bbb1afb11e1d516a125c65
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2024-10-02 02:07:50 +0530

    remain

---
+++ 17.Divide&Conquer/quick_sort.java
@@ -0,0 +1,20 @@
+public class quick_sort {
+
+    public static void printArr(int arr[]){
+        for(int i=0; i<arr.length; i++){
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    public static void quickSort(int arr[], int si, int ei){
+
+
+        //pivot = last element
+        
+    }
+    public static void main(String[] args) {
+        int arr [] = {6,3,9,8,2,5};
+        quickSort(arr,0, arr.length-1);
+    }
+}
