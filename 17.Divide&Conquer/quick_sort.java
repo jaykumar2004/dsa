d54168fa9626def511c06adcab47dac0a5c7dc0e
Jay Kumar jangid <106320129+jaykumar2004@users.noreply.github.com>
2024-10-02 16:45:59 +0530

    quick sort

--- 17.Divide&Conquer/quick_sort.java
+++ 17.Divide&Conquer/quick_sort.java
@@ -1,20 +1,46 @@
 public class quick_sort {
 
-    public static void printArr(int arr[]){
-        for(int i=0; i<arr.length; i++){
+    public static void printArr(int arr[]) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
 
-    public static void quickSort(int arr[], int si, int ei){
-
-
-        //pivot = last element
-        
+    public static void quickSort(int arr[], int si, int ei) {
+        // base case:
+        if (si >= ei) {
+            return;
+        }
+        // pivot = last element
+        int pidx = partition(arr, si, ei);  // partition index
+        quickSort(arr, si, pidx - 1); // left part
+        quickSort(arr, pidx + 1, ei); // right part
     }
+
+    public static int partition(int arr[], int si, int ei) {
+        int pivot = arr[ei];
+        int i = si - 1; // to make place for elements smaller than pivot
+
+        for (int j = si; j < ei; j++) { // Use j < ei, not i < ei
+            if (arr[j] <= pivot) {
+                i++;
+                // swap
+                int temp = arr[j];
+                arr[j] = arr[i];
+                arr[i] = temp;
+            }
+        }
+        i++;
+        int temp = arr[ei];
+        arr[ei] = arr[i];
+        arr[i] = temp;
+        return i;
+    }
+
     public static void main(String[] args) {
-        int arr [] = {6,3,9,8,2,5};
-        quickSort(arr,0, arr.length-1);
+        int arr[] = {6, 3, 9, 8, 2, 5};
+        quickSort(arr, 0, arr.length - 1);
+        printArr(arr);
     }
 }
