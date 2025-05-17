267bf5e7b86e53386b59127303a039e7dddd4d6d
Jay Kumar Jangid <106320129+jaykumar2004@users.noreply.github.com>
2025-05-17 18:51:19 +0530

    advance patterns questions

---
+++ All Codes/advance_patterns.java
@@ -0,0 +1,111 @@
+import java.nio.channels.Pipe.SourceChannel;
+import java.util.*;
+
+public class advance_patterns {
+
+    public static void hollow_rectangle(int totRows, int totCols) {
+        for (int i = 1; i <= totRows; i++) {
+            for (int j = 1; j <= totCols; j++) {
+                if (i == 1 || i == totRows || j == 1 || j == totCols) {
+                    // boundary cells
+                    System.out.print("*");
+                } else {
+                    System.out.print(" ");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+    // inverted and rotated half pyramid
+    public static void invRotHalfPyr(int n) {
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // inverted half pyramid with numbers
+    public static void invHalfPyrNo(int n) {
+        for (int i = 1; i <= n; i++) {
+            // -inner loop for numbers
+            for (int j = 1; j <= n - i + 1; j++) {
+                System.out.print(j);
+            }
+            System.out.println();
+        }
+    }
+
+    // floyd's triangle
+    public static void floydsTri(int n) {
+        int counter = 1;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(counter + " ");
+                counter++;
+            }
+            System.out.println();
+        }
+    }
+
+    //01 triangle
+    public static void printZeroOne(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                if((i+j)%2==0){
+                    System.out.print("1");
+                }
+                else{
+                    System.out.print("0");
+                }
+            }
+            System.out.println();
+        }
+    }
+
+
+    //butterfly pattern
+    public static void butterflyPattern(int n){
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+        for(int i=n;i>=1;i--){
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            for(int j=1;j<=2*(n-i);j++){
+                System.out.print(" ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        // hollow_rectangle(4, 5);
+        // invRotHalfPyr(4);
+        // invHalfPyrNo(5);
+        // floydsTri(5);
+        // printZeroOne(5);
+        butterflyPattern(5);
+    }
+}
