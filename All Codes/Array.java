0df27eb2288d89e833a8668b856a4c18d26b0a5a
jaykumar2004 <jay859119@gmail.com>
2025-06-11 15:00:20 +0530

    dsa

--- All Codes/Array.java
+++ All Codes/Array.java
@@ -1,19 +1,48 @@
 public class Array {
 
-    public static void update(int marks[]){
-        for(int i=0;i<marks.length;i++){
+    public static void update(int marks[]) {
+        for (int i = 0; i < marks.length; i++) {
             marks[i] = marks[i] + 1;
         }
     }
 
-    public static void main(String[] args) {
-        int marks[] = {97,98,99};
-        update(marks);
 
-        //print the marks
-        for(int i=0;i<marks.length;i++){
-            System.out.print(marks[i]+" ");
+
+    //linear search
+    public static int linearSearch(int numbers[], int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
         }
-        System.out.println();
+        return -1;
     }
+
+
+    //largest number
+    public static void int(int numbers)
+
+
+    public static void main(String[] args) {
+
+        int numbers[] = {2, 4, 6, 8, 10, 12, 14, 16};
+        int key = 10;
+        int index = linearSearch(numbers, key);
+        if (index == -1) {
+            System.out.println("NOT FOUND");
+        } else {
+            System.out.println("KEY IS AT INDEX: " + index);
+        }
+
+
+    }
+//        int marks[] = {97,98,99};
+//        update(marks);
+//
+//        //print the marks
+//        for(int i=0;i<marks.length;i++){
+//            System.out.print(marks[i]+" ");
+//        }
+//        System.out.println();
+//    }
 }
